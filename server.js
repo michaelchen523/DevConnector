46ee6b8808fb3bdff87109d4cabd23288bab0ecb
michaelchen523 <michaelchen523@gmail.com>
2019-07-18 21:58:07 -0600

    Removed serverless

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { connectDB } = require('./config/db');
-const serverless = require('serverless-http');
 const path = require('path');
 
 const app = express();
