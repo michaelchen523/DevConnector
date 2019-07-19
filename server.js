eeb2065053bc4e88a7f5ee3fed745ba2c2d900b8
michaelchen523 <michaelchen523@gmail.com>
2019-07-18 19:44:16 -0600

    Add Experience works. Got rid of tests/serverless for now

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { connectDB, closeDB } = require('./config/db');
+const serverless = require('serverless-http');
+const path = require('path');
 
 const app = express();
 
@@ -9,26 +11,22 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT}`)
-);
+// Serve static assets in producution
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
 
-process.on('SIGTERM', () => {
-  console.info('SIGTERM signal received');
-  console.log('Closing http server.');
-  server.close(() => {
-    console.log('HTTP Server closed.');
-    closeDB();
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-});
+}
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
